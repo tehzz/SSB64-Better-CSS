b57f96d2f2537377790c1d37dd4d49ece73bee95
tehzz <zgamer@gmail.com>
2016-11-17 20:36:41 -0500

    Reorganization
    Move data files into "data" folder, and move ASM files into "text"
    folder

--- Better-CSS/src/results/results-more-chars.asm
+++ Better-CSS/src/results/results-more-chars.asm
@@ -3,11 +3,11 @@
 
 //---.data------------------------
 scope data {
-include "dma/char-logo-data.bass"
-include "dma/char-bgm.bass"
-include "dma/char-fgm.bass"
-include "dma/char-zoom.bass"
-include "dma/char-strings.bass"
+include "data/char-logo-data.bass"
+include "data/char-bgm.bass"
+include "data/char-fgm.bass"
+include "data/char-zoom.bass"
+include "data/char-strings.bass"
 }
 //---end .data--------------------
 
@@ -15,17 +15,17 @@ include "dma/char-strings.bass"
 align(4)
 scope text {
 // Load only the <=4 characters used in a battle
-include "dma/load-chars.asm"
+include "text/load-chars.asm"
 // Have variable sized array for winning character logo
-include "dma/more-char-logos.asm"
+include "text/more-char-logos.asm"
 // expand victory theme array for all characters
-include "dma/victory-theme.asm"
+include "text/victory-theme.asm"
 // expand victory theme array for all characters
-include "dma/announce-winner.asm"
+include "text/announce-winner.asm"
 // expand zoom factor array for all characters
-include "dma/model-zoom-floats.asm"
+include "text/model-zoom-floats.asm"
 // winning character string
-include "dma/write-winner-str.asm"
+include "text/write-winner-str.asm"
 
 // victory and chapping animation
 
