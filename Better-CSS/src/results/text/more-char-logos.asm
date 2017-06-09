831a0d0bc958f81d6f2368d9fa78c32c25156c00
tehzz <zgamer@gmail.com>
2017-06-09 14:38:53 -0400

    Use smashlogo for m.h. and polygons
    Probably want to re-write this routine anyways, but...

--- Better-CSS/src/results/text/more-char-logos.asm
+++ Better-CSS/src/results/text/more-char-logos.asm
@@ -11,9 +11,11 @@
 //---Begin Hook------------------------
 // All of this will be within the routine, so it's really one big hook..
 //---Register Map------------
-// t0 : winning character index
-//      array[char.index]
+// t0 : winning character index (<< 2)
 // t1 : array (logo, color, zoom) bases
+//      array[winner.char * 4]
+// t2 : 0x80000000 bitmask
+// t3 : ram addr of resource-0035.bin
 //---Stack Map---------------
 // First Array  : 0x00A0 + sp
 // Second Array : 0x0070 + sp
@@ -32,6 +34,7 @@ scope char_logos_hooks {
   // End 80132C58
   constant put_winner_on_stack(0x80132BC8)
   constant end_put_winner(0x80132C58)
+  constant ram_resource35(0x8013A058)
   // First, re-organize the beginning of the draw logo routine
 
 prologue:
@@ -44,25 +47,37 @@ grab_winning_char:
             b     put_winner_on_stack  // at 0xD4
             nop
 load_array_values:
-            lw    t0, 0x00D4(sp)      // grab character index
-            la    t1, data.char_logo_offsets
-            sll   at, t0, 0x2         // convert char index to word offset
-            addu  t0, t1, at
-            lw    t0, 0x0000(t0)
-            la    t1, data.char_logo_color_offsets
-            sw    t0, 0x00A0(sp)      // store logo DL offset at 0xA0(sp)
-            addu  t0, t1, at
-            lw    t0, 0x0000(t0)
-            la    t1, data.char_logo_zoom_offsets
-            sw    t0, 0x00A4(sp)      // store logo color offset at 0xA4
-            addu  t0, t1, at
-            lw    t0, 0x0000(t0)
-            sw    t0, 0x00AC(sp)      // store zoom offset at 0xAC
-            or    a0, r0, r0          // replacement instruct at 80132C54
+            lw    t0, 0x00D4(sp)      // grab winning character index
+      lwAddr(t3, ram_resource35, 0)
+            lui   t2, 0x8000          // load the mask for non-offset logo pointers
+            sll   t0, t0, 0x2         // convert char index to word offset
+      lwAddr(t1, data.char_logo_offsets, t0)
+            and   at, t2, t1          // check if msb is set (aka it's a pointer to RAM)
+            bnezl at, store_logo_offset
+            sub   t1, t1, t3          // abuse likely branching to skip instruction if mask == 0
+store_logo_offset:
+            sw    t1, 0x00A0(sp)      // store logo DL offset at 0xA0(sp)
+
+      lwAddr(t1, data.char_logo_color_offsets, t0)
+            and   at, t2, t1
+            bnezl at, store_logo_color_offset
+            sub   t1, t1, t3
+store_logo_color_offset:
+            sw    t1, 0x00A4(sp)      // store logo color offset at 0xA4
+
+            lwAddr(t1, data.char_logo_zoom_offsets, t0)
+            and   at, t2, t1
+            bnezl at, store_logo_zoom_offset
+            sub   t1, t1, t3
+store_logo_zoom_offset:
+            sw    t1, 0x00AC(sp)      // store zoom offset at 0xAC
+
+            or    a0, r0, r0          // replacement instruction from 80132C54
             or    a1, r0, r0          // replacement
             b     end_put_winner + 0x8
             addiu a2, r0, 0x0017      // replacement instruct
 
+  nopUntilPC(put_winner_on_stack, "Character logo nop")
 // End of mini-internal routine
 origin 0x151DF8
 base end_put_winner   //0x80132C58
@@ -72,6 +87,7 @@ base end_put_winner   //0x80132C58
 // First Array Load and Use
 origin 0x151E1C
 base 0x80132C7C
+load_char_logo_dl:
             lw    t7, 0x00A0(sp)
 
 //---Second Array Load and Use----
@@ -85,11 +101,13 @@ base 0x80132C7C
 //--------------------------------
 origin 0x151E68
 base 0x80132CC8
+load_color_dl:
             lw    t5, 0x00A4(sp)
 
 // Third Array Load and Use
 origin 0x151E8C
 base 0x80132CEC
+load_zoom_dl:
             lw    t3, 0x00AC(sp)
 }
 
